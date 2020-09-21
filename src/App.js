983083ef36f330d741b2e2911e05e2f13fecb100
AgentMrBig <agentmrbig@gmail.com>
2020-09-21 12:47:28 -0400

    react-router-dom 71

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/hompage/homepage.component';
 
-class App extends Component {
-  render() {
-    return <Homepage />;
-  }
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+function App (){
+  return (
+  
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  
+  );
+  
 }
 
 export default App;
