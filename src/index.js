8f13195318a97e95be0318ec12cb1ccbcd8f29b6
AgentMrBig <agentmrbig@gmail.com>
2020-09-28 02:29:43 -0400

    added cart state persistance with persist

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {
 } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -18,9 +19,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter >
-
-            <App props={store} />
-
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter >
     </Provider>, document.getElementById('root')
 )
